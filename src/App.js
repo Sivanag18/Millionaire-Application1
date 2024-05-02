91c217dcb5e1ffbcc5a54676268ab1694b4dc106
SivaNag Bhavanam <sivanagbhavanam@gmail.com>
2024-05-02 15:41:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,230 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import  React,{ useEffect, useMemo, useState } from "react";
+import Start from "./components/start";
+import Timer from "./components/Timer";
+import Trivia from "./components/Trivia";
 
-function App() {
+ export default function App() {
+  const [username, setUsername] = useState(null);
+  const [stop, setStop] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [earned, setEarned] = useState("₹ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Who painted the Mona Lisa?",
+      answers: [
+        {
+          text: "Vincent van Gogh",
+          correct: false,
+        },
+        {
+          text: "Leonardo da Vinci",
+          correct: true,
+        },
+        {
+          text: "Pablo Picasso",
+          correct: false,
+        },
+        {
+          text: "Michelangelo",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Which planet is known as the Red Planet?",
+      answers: [
+        {
+          text: "Venus",
+          correct: false,
+        },
+        {
+          text: "Mars",
+          correct: true,
+        },
+        {
+          text: "Jupiter",
+          correct: false,
+        },
+        {
+          text: "Mercury",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      answers: [
+        {
+          text: "William Shakespeare",
+          correct: true,
+        },
+        {
+          text: "Charles Dickens",
+          correct: false,
+        },
+        {
+          text: "Jane Austen",
+          correct: false,
+        },
+        {
+          text: "Emily Dickinson",
+          correct: false,
+        },
+      ],
+    }
+    
+  ];
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "₹ 100" },
+        { id: 2, amount: "₹ 200" },
+        { id: 3, amount: "₹ 300" },
+        { id: 4, amount: "₹ 500" },
+        { id: 5, amount: "₹ 1.000" },
+        { id: 6, amount: "₹ 2.000" },
+        { id: 7, amount: "₹ 4.000" },
+        { id: 8, amount: "₹ 8.000" },
+        { id: 9, amount: "₹ 16.000" },
+        { id: 10, amount: "₹ 32.000" },
+        { id: 11, amount: "₹ 64.000" },
+        { id: 12, amount: "₹ 125.000" },
+        { id: 13, amount: "₹ 250.000" },
+        { id: 14, amount: "₹ 500.000" },
+        { id: 15, amount: "₹ 1.000.000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {username ? (
+     
+        <>
+          <div className="main">
+            {stop ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setStop={setStop}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    setStop={setStop}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : ( 
+      <Start setUsername={setUsername} />
+    )}
     </div>
   );
+
 }
 
-export default App;
+
---
+++ src/components/start.jsx
@@ -0,0 +1,27 @@
+
+import { useRef } from "react";
+
+export default function Start({ setUsername }) {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUsername(inputRef.current.value);
+  };
+
+  return (
+
+
+    <div className="start">
+      <h1 style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "20px" }}>Millionaire Application</h1>
+
+      <input
+        className="startInput"
+        placeholder="enter your name"
+        ref={inputRef}
+      />
+      <button className="startButton" onClick={handleClick}>
+        Start
+      </button>
+    </div>
+  );
+}
